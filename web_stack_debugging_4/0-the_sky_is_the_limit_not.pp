fb1b59d624d5358d7d03df5fe341a28bb9d23542
eseosapku <e.uwagboe@alustudent.com>
2023-02-10 07:07:30 +0000

    comit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,5 @@
+#sky is the limit
+
 exec { 'greater lengths':
   onlyif   => 'test -e /etc/default/nginx',
   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
